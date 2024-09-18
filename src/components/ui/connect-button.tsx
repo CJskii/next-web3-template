f9721ea721dd48b64fc2a9b960fd7c400a52ac96
CJskii <czareczarek3@gmail.com>
2024-09-18 16:38:58 +0100

    feat: custom connect button

---
+++ src/components/ui/connect-button.tsx
@@ -0,0 +1,110 @@
+"use client";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Button } from "./button";
+import Image from "next/image";
+
+export const ConnectWalletButton = () => {
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        const ready = mounted && authenticationStatus !== "loading";
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus || authenticationStatus === "authenticated");
+        return (
+          <div
+            {...(!ready && {
+              "aria-hidden": true,
+              style: {
+                opacity: 0,
+                pointerEvents: "none",
+                userSelect: "none",
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <Button
+                    className="bg-gradient rounded-xl font-normal hover:opacity-90"
+                    onClick={openConnectModal}
+                    type="button"
+                  >
+                    Connect Wallet
+                  </Button>
+                );
+              }
+              if (chain.unsupported) {
+                return (
+                  <Button
+                    variant={"destructive"}
+                    onClick={openChainModal}
+                    type="button"
+                    className="rounded-xl"
+                  >
+                    Wrong network
+                  </Button>
+                );
+              }
+              return (
+                <div className="flex gap-2 max-md:flex-col-reverse md:justify-center md:items-center">
+                  <Button
+                    className="rounded-xl"
+                    variant={"outline"}
+                    onClick={openChainModal}
+                    style={{ display: "flex", alignItems: "center" }}
+                    type="button"
+                  >
+                    {chain.hasIcon && (
+                      <div
+                        style={{
+                          background: chain.iconBackground,
+                          width: 12,
+                          height: 12,
+                          borderRadius: 999,
+                          overflow: "hidden",
+                          marginRight: 4,
+                        }}
+                      >
+                        {chain.iconUrl && (
+                          <Image
+                            alt={chain.name ?? "Chain icon"}
+                            src={chain.iconUrl}
+                            width={12}
+                            height={12}
+                          />
+                        )}
+                      </div>
+                    )}
+                    {chain.name}
+                  </Button>
+                  <Button
+                    variant={"outline"}
+                    onClick={openAccountModal}
+                    className="bg-gradient rounded-xl font-normal hover:opacity-90"
+                    type="button"
+                  >
+                    {account.displayName}
+                    {account.displayBalance
+                      ? ` (${account.displayBalance})`
+                      : ""}
+                  </Button>
+                </div>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
+};
