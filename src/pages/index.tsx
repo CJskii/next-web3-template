7f92e55a419648ffe5fbcdd6412812ec01f83035
CJskii <czareczarek3@gmail.com>
2024-09-19 15:42:13 +0100

    refactor: landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,26 +4,58 @@ import { NextPage } from "next/types";
 import React from "react";
 import { Typography } from "@/components/ui/typography";
 import Link from "next/link";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { GitFork, StarIcon } from "lucide-react";
+import { GitHubIcon } from "@/assets/icons/social";
 
 const HomePage: NextPage = () => {
   return (
     <PageLayout title="Homepage" description="Welcome to next-web-template">
-      <Typography variant="h1">Welcome to next-web3-template</Typography>
-      <Typography variant="h2">
+      <Typography variant="h1" className="text-center">
+        Welcome to next-web3-template
+      </Typography>
+      <Typography variant="h2" className="text-center">
         A template for building web3 native applications
       </Typography>
-      <div className="flex justify-center items-center gap-12 my-4">
-        <GitHubLogoIcon className="w-12 h-12 inline-block" />
-      </div>
 
-      {/* TODO: Add button to view repo on github + star repo + fork repo */}
       <Link
         href="https://github.com/new?template_name=next-web3-template&template_owner=CJskii"
         target="_blank"
+        className="my-12"
       >
-        <Button>Get Started</Button>
+        <Button className="gap-2">
+          <GitFork />
+          Get Started
+        </Button>
       </Link>
+
+      <div className="flex flex-col items-center justify-center gap-6 my-8">
+        <Typography variant="lead" className="text-center">
+          If you find this template helpful, please consider giving it a star on
+          GitHub to support the project!
+        </Typography>
+
+        <div className="flex gap-4">
+          <Link
+            href="https://github.com/CJskii/next-web3-template"
+            target="_blank"
+          >
+            <Button className="gap-2" variant={"link"}>
+              <GitHubIcon />
+              View on GitHub
+            </Button>
+          </Link>
+
+          <Link
+            href="https://github.com/CJskii/next-web3-template/stargazers"
+            target="_blank"
+          >
+            <Button className="gap-2" variant={"link"}>
+              <StarIcon />
+              Give a Star
+            </Button>
+          </Link>
+        </div>
+      </div>
     </PageLayout>
   );
 };
